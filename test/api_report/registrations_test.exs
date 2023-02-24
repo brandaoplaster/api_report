41df449cb8d441ed2221cc9a03f2ee5fae20d28d
brandaoplaster <brandaoplaster@gmail.com>
2023-02-24 18:32:41 -0300

    test: Adding tests to registrations

---
+++ test/api_report/registrations_test.exs
@@ -0,0 +1,32 @@
+defmodule ApiReport.RegistrationsTest do
+  use ApiReport.DataCase
+
+  import ApiReport.Factory
+
+  alias ApiReport.Registrations.Registration
+  alias ApiReport.Registrations
+
+  describe "registration" do
+    test "create_registration/1 with valid data creates a registration" do
+      partner = insert(:partner)
+      expected = params_for(:registration, %{email: "jhon@gmail.com", partner_id: partner.id})
+
+      assert {:ok, %Registration{} = registration} = Registrations.create_registration(expected)
+      assert registration.email == expected.email
+    end
+
+    test "create_registration/1 with invalid data returns error changeset" do
+      partner = insert(:partner)
+      expected = params_for(:registration, %{email: "", partner_id: partner.id})
+      assert {:error, %Ecto.Changeset{}} = Registrations.create_registration(expected)
+    end
+
+    test "create_registration/1 without partner_id returns error MatchError" do
+      expected = params_for(:registration, %{email: ""})
+
+      assert_raise MatchError, fn ->
+        Registrations.create_registration(expected)
+      end
+    end
+  end
+end
