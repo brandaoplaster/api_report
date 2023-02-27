b7704f7b0f94b5735ebeb18955c3d2f57524f714
brandaoplaster <brandaoplaster@gmail.com>
2023-02-26 22:14:48 -0300

    feat: Adding controller and view for partner

--- lib/api_report/partners.ex
+++ lib/api_report/partners.ex
@@ -7,16 +7,30 @@ defmodule ApiReport.Partners do
 
   alias ApiReport.Repo
   alias ApiReport.Partners.Partner
+  alias ApiReport.ErrorHandling.Error
 
   def create_partner(attrs \\ %{}) do
     %Partner{}
     |> Partner.changeset(attrs)
     |> Repo.insert()
+    |> handle_insert()
   end
 
   def list_partners do
     Repo.all(Partner)
   end
 
-  def get_partner!(id), do: Repo.get!(Partner, id)
+  def get_partner(id) do
+    with %Partner{} = partner <- Repo.get(Partner, id) do
+      {:ok, partner}
+    else
+      nil -> {:error, Error.build_partner_not_found_error()}
+    end
+  end
+
+  defp handle_insert({:ok, %Partner{}} = partner), do: partner
+
+  defp handle_insert({:error, changeset}) do
+    {:error, Error.build(:bad_request, changeset)}
+  end
 end
