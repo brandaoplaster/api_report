3259a8cff718fddb37b2c8a6aa83864faa06670b
brandaoplaster <brandaoplaster@gmail.com>
2023-03-01 23:25:39 -0300

    feat: Adding queries to generate report

--- lib/api_report/registrations.ex
+++ lib/api_report/registrations.ex
@@ -6,7 +6,26 @@ defmodule ApiReport.Registrations do
   import Ecto.Query, warn: false
 
   alias ApiReport.{Partners, Registrations, Repo}
+  alias ApiReport.Partners.Partner
   alias ApiReport.Registrations.Registration
+
+  defmacro dates_between(field, initial, final) do
+    quote do:
+            fragment(
+              "? BETWEEN ? AND ?",
+              unquote(field),
+              type(unquote(initial), :date),
+              type(unquote(final), :date)
+            )
+  end
+
+  defmacro date_trunc(field) do
+    quote do: fragment("DATE_TRUNC('DAY', ?)", unquote(field))
+  end
+
+  defmacro format_date(field) do
+    quote do: fragment("TO_CHAR(?, 'DD/MM/YYYY')", unquote(field))
+  end
 
   def create_registration(attrs \\ %{}) do
     %{"partner_id" => partner_id} = attrs
@@ -16,5 +35,36 @@ defmodule ApiReport.Registrations do
     |> Ecto.build_assoc(:registrations)
     |> Registration.changeset(attrs)
     |> Repo.insert()
+  end
+
+  def daily_registrations(%{"start_date" => start_date, "end_date" => end_date}) do
+    Registration
+    |> join(:inner, [r], p in Partner, on: r.partner_id == p.id)
+    |> where([r, p], dates_between(date_trunc(r.inserted_at), ^start_date, ^end_date))
+    |> group_by([r, p], [format_date(r.inserted_at), r.name, r.cpf, r.email])
+    |> select([r, p], %{
+      date: format_date(r.inserted_at),
+      name: r.name,
+      cpf: r.cpf,
+      email: r.email,
+      amount_of_records: count("*")
+    })
+    |> Repo.all()
+  end
+
+  def daily_registrations_by_partner(%{"start_date" => start_date, "end_date" => end_date}) do
+    Registration
+    |> join(:inner, [r], p in Partner, on: r.partner_id == p.id)
+    |> where([r, p], dates_between(date_trunc(r.inserted_at), ^start_date, ^end_date))
+    |> group_by([r, p], [p.name, format_date(r.inserted_at), r.name, r.cpf, r.email])
+    |> select([r, p], %{
+      partner_name: p.name,
+      date: format_date(r.inserted_at),
+      name: r.name,
+      cpf: r.cpf,
+      email: r.email,
+      amount_of_records: count("*")
+    })
+    |> Repo.all()
   end
 end
