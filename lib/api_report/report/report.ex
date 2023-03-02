3613874022fa829f8feaaab9e059a099c2f7c9f3
brandaoplaster <brandaoplaster@gmail.com>
2023-03-02 00:39:15 -0300

    feat: Adding logic to generate report

---
+++ lib/api_report/report/report.ex
@@ -0,0 +1,51 @@
+defmodule ApiReport.Report.Report do
+  alias ApiReport.Registrations
+  alias ApiReport.Report.Csv
+  alias ApiReport.ErrorHandling.Error
+
+  def generate_report(%{"report_name" => "DailyRegistrations"} = params) do
+    case validate_filters(params) do
+      {:ok, params} ->
+        daily_registrations(params)
+
+      {:error, _} ->
+        {:error, Error.build_invalid_filter_error()}
+    end
+  end
+
+  def generate_report(%{"report_name" => "DailyRegistrationsByPartner"} = params) do
+    case validate_filters(params) do
+      {:ok, params} ->
+        daily_registrations_by_partner(params)
+
+      {:error, _} ->
+        {:error, Error.build_invalid_filter_error()}
+    end
+  end
+
+  def generate_report(%{"report_name" => _} = _params) do
+    {:error, Error.build_report_not_found_error()}
+  end
+
+  defp daily_registrations(params) do
+    params
+    |> Registrations.daily_registrations()
+    |> Csv.build_daily_registrations()
+  end
+
+  defp daily_registrations_by_partner(params) do
+    params
+    |> Registrations.daily_registrations_by_partner()
+    |> Csv.build_daily_registrations_by_partner()
+  end
+
+  defp validate_filters(params) do
+    case Map.has_key?(params, "start_date") and Map.has_key?(params, "end_date") do
+      true ->
+        {:ok, params}
+
+      false ->
+        {:error, "invalid filter"}
+    end
+  end
+end
